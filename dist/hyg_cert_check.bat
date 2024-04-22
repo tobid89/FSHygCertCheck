ed7dc336cdc01a4c57d93da38b293982cb67fd4b
drestob89 <14858531+tobid89@users.noreply.github.com>
2024-04-22 19:17:54 +0200

    Disabled "onefile" option of pyinstaller

--- dist/hyg_cert_check.bat
+++ dist/hyg_cert_check.bat
@@ -2,6 +2,6 @@
 REM Adapt the last line of this file with your personal information as described in the following.
 REM Replace the placeholder 'foodsharing-login-email' and 'foodsharing-login-password' with your login credential for foodsharing.de
 REM Replace the placeholder 'hygiene-certificate-liste-password' with the password for the hygiene certificate list posted in the from of the group "Hygienezertifikate Hannover"
-REM Example: .\hyg_cert_check.exe heins.peter@tonline.de heinsPaswort123 HygeneListePasswort
+REM Example: .\hyg_cert_check\hyg_cert_check.exe heins.peter@tonline.de heinsPaswort123 HygeneListePasswort
 
-.\hyg_cert_check.exe foodsharing-login-email foodsharing-login-password hygiene-certificate-liste-password
+.\hyg_cert_check\hyg_cert_check.exe foodsharing-login-email foodsharing-login-password hygiene-certificate-liste-password
